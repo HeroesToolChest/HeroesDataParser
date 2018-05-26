e348d397f37f64a5fcb563aa122e924b0610c246
Kevin Oliva <kevin.k.oliva@gmail.com>
2018-05-26 00:11:36 -0500

    Updated hero verification

--- HeroesData.Parser/VerifyHeroData.cs
+++ HeroesData.Parser/VerifyHeroData.cs
@@ -64,76 +64,14 @@ namespace HeroesData.Parser
                 if (!hero.ReleaseDate.HasValue)
                     AddWarning($"{nameof(hero.ReleaseDate)} is null");
 
-                foreach (var weapon in hero.Weapons)
-                {
-                    if (weapon.Damage <= 0)
-                        AddWarning($"{weapon.WeaponNameId} {nameof(weapon.Damage)} is 0");
-
-                    if (weapon.Period <= 0)
-                        AddWarning($"{weapon.WeaponNameId} {nameof(weapon.Period)} is 0");
-
-                    if (weapon.Range <= 0)
-                        AddWarning($"{weapon.WeaponNameId} {nameof(weapon.Range)} is 0");
-
-                    if (weapon.DamageScaling <= 0)
-                        AddWarning($"{weapon.WeaponNameId} {nameof(weapon.DamageScaling)} is 0");
-                }
+                VerifyWeapons(hero);
 
                 if (hero.Weapons.Count < 1)
                     AddWarning("has no weapons");
                 else if (hero.Weapons.Count > 1)
                     AddWarning("has more than 1 weapon");
 
-                foreach (var ability in hero.Abilities)
-                {
-                    if (string.IsNullOrEmpty(ability.Value.IconFileName))
-                        AddWarning($"[{ability.Key}] {nameof(ability.Value.IconFileName)} is null or empty");
-
-                    if (string.IsNullOrEmpty(ability.Value.Name))
-                        AddWarning($"[{ability.Key}] {nameof(ability.Value.Name)} is null or empty");
-
-                    if (string.IsNullOrEmpty(ability.Value.ReferenceNameId))
-                        AddWarning($"[{ability.Key}] {nameof(ability.Value.ReferenceNameId)} is null or empty");
-
-                    if (string.IsNullOrEmpty(ability.Value.FullTooltipNameId))
-                        AddWarning($"[{ability.Key}] {nameof(ability.Value.FullTooltipNameId)} is null or empty");
-
-                    if (string.IsNullOrEmpty(ability.Value.ShortTooltipNameId))
-                        AddWarning($"[{ability.Key}] {nameof(ability.Value.ShortTooltipNameId)} is null or empty");
-
-                    if (ability.Value.Tooltip.Cooldown.CooldownValue < 0)
-                        AddWarning($"[{ability.Key}] {nameof(ability.Value.Tooltip.Cooldown.CooldownValue)} is negative.");
-
-                    if (ability.Value.Tooltip.Energy.EnergyCost < 0)
-                        AddWarning($"[{ability.Key}] {nameof(ability.Value.Tooltip.Energy.EnergyCost)} is negative.");
-
-                    if (ability.Value.Tooltip.Life.LifeCost < 0)
-                        AddWarning($"[{ability.Key}] {nameof(ability.Value.Tooltip.Life.LifeCost)} is negative.");
-
-                    if (string.IsNullOrEmpty(ability.Value.Tooltip.ShortTooltip?.RawDescription))
-                        AddWarning($"[{ability.Key}] {nameof(ability.Value.Tooltip.ShortTooltip)} is null or empty");
-
-                    if (string.IsNullOrEmpty(ability.Value.Tooltip.FullTooltip?.RawDescription))
-                        AddWarning($"[{ability.Key}] {nameof(ability.Value.Tooltip.FullTooltip)} is null or empty");
-
-                    if (string.IsNullOrEmpty(ability.Value.Tooltip.FullTooltip?.PlainText))
-                        AddWarning($"[{ability.Key}] {nameof(ability.Value.Tooltip.FullTooltip)}.{nameof(ability.Value.Tooltip.FullTooltip.PlainText)} is null or empty");
-
-                    if (string.IsNullOrEmpty(ability.Value.Tooltip.FullTooltip?.PlainTextWithNewlines))
-                        AddWarning($"[{ability.Key}] {nameof(ability.Value.Tooltip.FullTooltip)}.{nameof(ability.Value.Tooltip.FullTooltip.PlainTextWithScaling)} is null or empty");
-
-                    if (string.IsNullOrEmpty(ability.Value.Tooltip.FullTooltip?.PlainTextWithScaling))
-                        AddWarning($"[{ability.Key}] {nameof(ability.Value.Tooltip.FullTooltip)}.{nameof(ability.Value.Tooltip.FullTooltip.PlainTextWithScaling)} is null or empty");
-
-                    if (string.IsNullOrEmpty(ability.Value.Tooltip.FullTooltip?.PlainTextWithScalingWithNewlines))
-                        AddWarning($"[{ability.Key}] {nameof(ability.Value.Tooltip.FullTooltip)}.{nameof(ability.Value.Tooltip.FullTooltip.PlainTextWithScalingWithNewlines)} is null or empty");
-
-                    if (string.IsNullOrEmpty(ability.Value.Tooltip.FullTooltip?.ColoredText))
-                        AddWarning($"[{ability.Key}] {nameof(ability.Value.Tooltip.FullTooltip)}.{nameof(ability.Value.Tooltip.FullTooltip.ColoredText)} is null or empty");
-
-                    if (string.IsNullOrEmpty(ability.Value.Tooltip.FullTooltip?.ColoredTextWithScaling))
-                        AddWarning($"[{ability.Key}] {nameof(ability.Value.Tooltip.FullTooltip)}.{nameof(ability.Value.Tooltip.FullTooltip.ColoredTextWithScaling)} is null or empty");
-                }
+                VerifyAbilities(hero);
 
                 foreach (var talent in hero.Talents)
                 {
@@ -208,25 +146,68 @@ namespace HeroesData.Parser
                         if (additionalUnit.Speed <= 0)
                             AddWarning($"[{additionalUnit.Name}] {nameof(hero.Speed)} is 0");
 
-                        foreach (var weapon in additionalUnit.Weapons)
-                        {
-                            if (weapon.Damage <= 0)
-                                AddWarning($"[{additionalUnit.Name}] {weapon.WeaponNameId} {nameof(weapon.Damage)} is 0");
-
-                            if (weapon.Period <= 0)
-                                AddWarning($"[{additionalUnit.Name}] {weapon.WeaponNameId} {nameof(weapon.Period)} is 0");
-
-                            if (weapon.Range <= 0)
-                                AddWarning($"[{additionalUnit.Name}] {weapon.WeaponNameId} {nameof(weapon.Range)} is 0");
-
-                            if (weapon.DamageScaling <= 0)
-                                AddWarning($"[{additionalUnit.Name}] {weapon.WeaponNameId} {nameof(weapon.DamageScaling)} is 0");
-                        }
+                        VerifyWeapons(hero);
+                        VerifyAbilities(hero);
                     }
                 }
             }
 
+            Warnings.Sort();
             return Warnings;
+        }
+
+        private static void VerifyWeapons(Unit unit)
+        {
+            foreach (var weapon in unit.Weapons)
+            {
+                if (weapon.Damage <= 0)
+                    AddWarning($"{weapon.WeaponNameId} {nameof(weapon.Damage)} is 0");
+
+                if (weapon.Period <= 0)
+                    AddWarning($"{weapon.WeaponNameId} {nameof(weapon.Period)} is 0");
+
+                if (weapon.Range <= 0)
+                    AddWarning($"{weapon.WeaponNameId} {nameof(weapon.Range)} is 0");
+
+                if (weapon.DamageScaling <= 0)
+                    AddWarning($"{weapon.WeaponNameId} {nameof(weapon.DamageScaling)} is 0");
+            }
+        }
+
+        private static void VerifyAbilities(Unit unit)
+        {
+            foreach (var ability in unit.Abilities)
+            {
+                if (string.IsNullOrEmpty(ability.Value.IconFileName))
+                    AddWarning($"[{ability.Key}] {nameof(ability.Value.IconFileName)} is null or empty");
+
+                if (string.IsNullOrEmpty(ability.Value.Name))
+                    AddWarning($"[{ability.Key}] {nameof(ability.Value.Name)} is null or empty");
+
+                if (string.IsNullOrEmpty(ability.Value.ReferenceNameId))
+                    AddWarning($"[{ability.Key}] {nameof(ability.Value.ReferenceNameId)} is null or empty");
+
+                if (string.IsNullOrEmpty(ability.Value.FullTooltipNameId))
+                    AddWarning($"[{ability.Key}] {nameof(ability.Value.FullTooltipNameId)} is null or empty");
+
+                if (string.IsNullOrEmpty(ability.Value.ShortTooltipNameId))
+                    AddWarning($"[{ability.Key}] {nameof(ability.Value.ShortTooltipNameId)} is null or empty");
+
+                if (ability.Value.Tooltip.Cooldown.CooldownValue < 0)
+                    AddWarning($"[{ability.Key}] {nameof(ability.Value.Tooltip.Cooldown.CooldownValue)} is negative.");
+
+                if (ability.Value.Tooltip.Energy.EnergyCost < 0)
+                    AddWarning($"[{ability.Key}] {nameof(ability.Value.Tooltip.Energy.EnergyCost)} is negative.");
+
+                if (ability.Value.Tooltip.Life.LifeCost < 0)
+                    AddWarning($"[{ability.Key}] {nameof(ability.Value.Tooltip.Life.LifeCost)} is negative.");
+
+                if (string.IsNullOrEmpty(ability.Value.Tooltip.ShortTooltip?.RawDescription))
+                    AddWarning($"[{ability.Key}] {nameof(ability.Value.Tooltip.ShortTooltip)} is null or empty");
+
+                if (string.IsNullOrEmpty(ability.Value.Tooltip.FullTooltip?.RawDescription))
+                    AddWarning($"[{ability.Key}] {nameof(ability.Value.Tooltip.FullTooltip)} is null or empty");
+            }
         }
 
         private static void AddWarning(string message)
