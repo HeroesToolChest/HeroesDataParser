ac2bc0bf8cbcb81f0f12ce91ab75d549ecb46036
Kevin Oliva <kevin.k.oliva@gmail.com>
2018-06-25 16:54:35 -0500

    Unit test for pathextensions

---
+++ tests/HeroesData.Parser.Tests/PathExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+using Xunit;
+
+namespace HeroesData.Parser.Tests
+{
+    public class PathExtensionsTests
+    {
+        [Fact]
+        public void GetWindowsFilePathTest()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                Assert.Equal(@"test\to\filePath\", PathExtensions.GetFilePath(@"test\to\filePath\"));
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                Assert.Equal("test/to/filePath/", PathExtensions.GetFilePath(@"test\to\filePath\"));
+        }
+    }
+}
