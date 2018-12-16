eda229fcf8b5c376889afb1fc3b733c61516bbfb
Kevin Oliva <kevin.k.oliva@gmail.com>
2018-12-16 12:28:01 -0600

    Lowecase _stormHero

--- HeroesData.Parser/StormHero.cs
+++ HeroesData.Parser/StormHero.cs
@@ -2,7 +2,7 @@
 {
     public class StormHero
     {
-        public const string CUnitId = "_StormHero";
-        public const string CHeroId = "_StormHero";
+        public const string CUnitId = "_stormHero";
+        public const string CHeroId = "_stormHero";
     }
 }
