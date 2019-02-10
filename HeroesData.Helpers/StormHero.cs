eeef28c415c281847081a3d16034631d0691498b
Kevin Oliva <kevin.k.oliva@gmail.com>
2019-02-10 14:41:30 -0600

    Moved some classes from parser to helpers

---
+++ HeroesData.Helpers/StormHero.cs
@@ -0,0 +1,11 @@
+﻿namespace HeroesData.Helpers
+{
+    /// <summary>
+    /// Default base hero used for common attributes for all heroes.
+    /// </summary>
+    public class StormHero
+    {
+        public const string CUnitId = "_stormhero";
+        public const string CHeroId = "_stormhero";
+    }
+}
