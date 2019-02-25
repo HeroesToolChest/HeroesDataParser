90297923f6195ecc661f2330d772a068a8209908
Kevin Oliva <kevin.k.oliva@gmail.com>
2019-02-25 17:40:40 -0600

    Added placeholder override files

---
+++ HeroesData.Parser/Overrides/HeroSkinOverrideLoader.cs
@@ -0,0 +1,30 @@
+﻿using HeroesData.Loader.XmlGameData;
+using HeroesData.Parser.Overrides.DataOverrides;
+using System;
+using System.Xml.Linq;
+
+namespace HeroesData.Parser.Overrides
+{
+    public class HeroSkinOverrideLoader : OverrideLoaderBase<HeroSkinDataOverride>, IOverrideLoader
+    {
+        public HeroSkinOverrideLoader(GameData gameData, int? hotsBuild)
+            : base(gameData, hotsBuild)
+        {
+        }
+
+        protected override string OverrideFileName
+        {
+            get
+            {
+                return $"heroskin-{base.OverrideFileName}";
+            }
+        }
+
+        protected override string OverrideElementName => "CSkin";
+
+        protected override void SetOverride(XElement element)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
