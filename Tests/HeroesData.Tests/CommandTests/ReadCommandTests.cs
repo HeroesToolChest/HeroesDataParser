1afa992f01ec8c368ad6ce14efdb43108dbcf569
Kevin Oliva <kevin.k.oliva@gmail.com>
2020-04-25 23:36:02 -0500

    Simplify using statement

--- Tests/HeroesData.Tests/CommandTests/ReadCommandTests.cs
+++ Tests/HeroesData.Tests/CommandTests/ReadCommandTests.cs
@@ -12,34 +12,31 @@ namespace HeroesData.Tests.CommandTests
         [TestMethod]
         public void BasicNoOptionsTest()
         {
-            using (StringWriter writer = new StringWriter())
-            {
-                Console.SetOut(writer);
-                Console.SetError(writer);
+            using StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+            Console.SetError(writer);
 
-                Program.Main(new string[] { "read", Path.Combine("CommandTests", "Test.txt") });
+            Program.Main(new string[] { "read", Path.Combine("CommandTests", "Test.txt") });
 
-                List<string> lines = writer.ToString().Split(Environment.NewLine).ToList();
+            List<string> lines = writer.ToString().Split(Environment.NewLine).ToList();
 
-                Assert.AreEqual($"CommandTests{Path.DirectorySeparatorChar}Test.txt", lines[0].Split(' ')[0]);
-                Assert.AreEqual("TestLine", lines[2]);
-            }
+            Assert.AreEqual($"CommandTests{Path.DirectorySeparatorChar}Test.txt", lines[0].Split(' ')[0]);
+            Assert.AreEqual("TestLine", lines[2]);
         }
 
         [TestMethod]
         public void BasicNoArgumentTest()
         {
-            using (StringWriter writer = new StringWriter())
-            {
-                Console.SetOut(writer);
-                Console.SetError(writer);
+            using StringWriter writer = new StringWriter();
 
-                Program.Main(new string[] { "read" });
+            Console.SetOut(writer);
+            Console.SetError(writer);
 
-                List<string> lines = writer.ToString().Split(Environment.NewLine).ToList();
+            Program.Main(new string[] { "read" });
 
-                Assert.AreEqual("Must provide a file name.", lines[0]);
-            }
+            List<string> lines = writer.ToString().Split(Environment.NewLine).ToList();
+
+            Assert.AreEqual("Must provide a file name.", lines[0]);
         }
     }
 }
