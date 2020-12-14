a88a2f4ddc5d9e82ac4b21db1327e67d2f495e10
Kevin Oliva <kevin.k.oliva@gmail.com>
2020-12-13 18:06:37 -0600

    Removed some validation

--- HeroesData/ExtractorData/DataBundle.cs
+++ HeroesData/ExtractorData/DataBundle.cs
@@ -17,23 +17,14 @@ namespace HeroesData.ExtractorData
             if (bundle is null)
                 return;
 
-            if (string.IsNullOrEmpty(bundle.Name))
-                AddWarning($"{nameof(bundle.Name)} is empty");
-
             if (string.IsNullOrEmpty(bundle.Id))
                 AddWarning($"{nameof(bundle.Id)} is empty");
 
             if (string.IsNullOrEmpty(bundle.HyperlinkId))
                 AddWarning($"{nameof(bundle.HyperlinkId)} is empty");
 
-            if (string.IsNullOrEmpty(bundle.SortName))
-                AddWarning($"{nameof(bundle.SortName)} is empty");
-
             if (!bundle.ReleaseDate.HasValue)
                 AddWarning($"{nameof(bundle.ReleaseDate)} is null");
-
-            if (string.IsNullOrEmpty(bundle.ImageFileName))
-                AddWarning($"{nameof(bundle.ImageFileName)} is empty");
         }
     }
 }
