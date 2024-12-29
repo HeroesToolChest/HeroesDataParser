bf723ae352884fd83c4c6545eeef944be1506e57
Kevin Oliva <kevin.k.oliva@gmail.com>
2024-12-29 14:32:03 -0600

    Added in dds converter

---
+++ HeroesDataParser/DDSImage.cs
@@ -0,0 +1,347 @@
+﻿using Pfim;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Gif;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace HeroesDataParser;
+
+public sealed class DDSImage : IDisposable
+{
+    private readonly IImage _ddsImageFile;
+
+    public DDSImage(string file)
+    {
+        _ddsImageFile = Pfimage.FromFile(file, new PfimConfig(decompress: true));
+    }
+
+    public DDSImage(Stream stream)
+    {
+        _ddsImageFile = Dds.Create(stream, new PfimConfig(decompress: true));
+    }
+
+    public int Width => _ddsImageFile.Width;
+
+    public int Height => _ddsImageFile.Height;
+
+    public void Dispose()
+    {
+        _ddsImageFile.Dispose();
+    }
+
+    /// <summary>
+    /// Writes the image to the file path.
+    /// </summary>
+    /// <param name="file">The file path the image will be written to. The file extension determine to conversion to perform.</param>
+    public void Save(string file)
+    {
+        if (_ddsImageFile.Format == ImageFormat.Rgba32)
+        {
+            Save<Bgra32>(file);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.Rgb24)
+        {
+            Save<Bgr24>(file);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.Rgba16)
+        {
+            Save<Bgra4444>(file);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.R5g5b5)
+        {
+            // Turn the alpha channel on for image sharp
+            for (int i = 1; i < _ddsImageFile.Data.Length; i += 2)
+            {
+                _ddsImageFile.Data[i] |= 128;
+            }
+
+            Save<Bgra5551>(file);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.R5g5b5a1)
+        {
+            Save<Bgra5551>(file);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.R5g6b5)
+        {
+            Save<Bgr565>(file);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.Rgb8)
+        {
+            throw new Exception($"Unsupported pixel format ({_ddsImageFile.Format})");
+        }
+        else
+        {
+            throw new Exception($"Unsupported pixel format ({_ddsImageFile.Format})");
+        }
+    }
+
+    /// <summary>
+    /// Writes the image to the file path.
+    /// </summary>
+    /// <param name="file">The file path the image will be written to.</param>
+    /// <param name="point">The coordinates where the image will be cropped from.</param>
+    /// <param name="size">The size of the new image.</param>
+    public void Save(string file, Point point, Size size)
+    {
+        if (_ddsImageFile.Format == ImageFormat.Rgba32)
+        {
+            Save<Bgra32>(file, point, size);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.Rgb24)
+        {
+            Save<Bgr24>(file, point, size);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.Rgba16)
+        {
+            Save<Bgra4444>(file, point, size);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.R5g5b5)
+        {
+            // Turn the alpha channel on for image sharp
+            for (int i = 1; i < _ddsImageFile.Data.Length; i += 2)
+            {
+                _ddsImageFile.Data[i] |= 128;
+            }
+
+            Save<Bgra5551>(file, point, size);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.R5g5b5a1)
+        {
+            Save<Bgra5551>(file, point, size);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.R5g6b5)
+        {
+            Save<Bgr565>(file, point, size);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.Rgb8)
+        {
+            throw new Exception($"Unsupported pixel format ({_ddsImageFile.Format})");
+        }
+        else
+        {
+            throw new Exception($"Unsupported pixel format ({_ddsImageFile.Format})");
+        }
+    }
+
+    /// <summary>
+    /// Writes the image to the file path as a gif.
+    /// </summary>
+    /// <param name="file">The file path the image will be written to.</param>
+    /// <param name="size">The size of the new image.</param>
+    /// <param name="innerSize">The size of the inner images on the base image.</param>
+    /// <param name="frames">The amount of frames.</param>
+    /// <param name="frameDelay">The delay of each frame.</param>
+    public void SaveAsGif(string file, Size size, Size innerSize, int frames, int frameDelay)
+    {
+        if (Path.GetExtension(file) != ".gif")
+            throw new Exception("File is not a gif");
+
+        if (_ddsImageFile.Format == ImageFormat.Rgba32)
+        {
+            SaveAsGif<Bgra32>(file, size, innerSize, frames, frameDelay);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.Rgb24)
+        {
+            SaveAsGif<Bgr24>(file, size, innerSize, frames, frameDelay);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.Rgba16)
+        {
+            SaveAsGif<Bgra4444>(file, size, innerSize, frames, frameDelay);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.R5g5b5)
+        {
+            // Turn the alpha channel on for image sharp
+            for (int i = 1; i < _ddsImageFile.Data.Length; i += 2)
+            {
+                _ddsImageFile.Data[i] |= 128;
+            }
+
+            SaveAsGif<Bgra5551>(file, size, innerSize, frames, frameDelay);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.R5g5b5a1)
+        {
+            SaveAsGif<Bgra5551>(file, size, innerSize, frames, frameDelay);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.R5g6b5)
+        {
+            SaveAsGif<Bgr565>(file, size, innerSize, frames, frameDelay);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.Rgb8)
+        {
+            throw new Exception($"Unsupported pixel format ({_ddsImageFile.Format})");
+        }
+        else
+        {
+            throw new Exception($"Unsupported pixel format ({_ddsImageFile.Format})");
+        }
+    }
+
+    /// <summary>
+    /// Writes the image to the file path as an apng.
+    /// </summary>
+    /// <param name="file">The file path the image will be written to.</param>
+    /// <param name="size">The size of the new image.</param>
+    /// <param name="innerSize">The size of the inner images on the base image.</param>
+    /// <param name="frames">The amount of frames.</param>
+    /// <param name="frameDelay">The delay of each frame.</param>
+    public void SaveAsAPNG(string file, Size size, Size innerSize, int frames, int frameDelay)
+    {
+        if (_ddsImageFile.Format == ImageFormat.Rgba32)
+        {
+            SaveAsAPNG<Bgra32>(file, size, innerSize, frames, frameDelay);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.Rgb24)
+        {
+            SaveAsAPNG<Bgr24>(file, size, innerSize, frames, frameDelay);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.Rgba16)
+        {
+            SaveAsAPNG<Bgra4444>(file, size, innerSize, frames, frameDelay);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.R5g5b5)
+        {
+            // Turn the alpha channel on for image sharp
+            for (int i = 1; i < _ddsImageFile.Data.Length; i += 2)
+            {
+                _ddsImageFile.Data[i] |= 128;
+            }
+
+            SaveAsAPNG<Bgra5551>(file, size, innerSize, frames, frameDelay);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.R5g5b5a1)
+        {
+            SaveAsAPNG<Bgra5551>(file, size, innerSize, frames, frameDelay);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.R5g6b5)
+        {
+            SaveAsAPNG<Bgr565>(file, size, innerSize, frames, frameDelay);
+        }
+        else if (_ddsImageFile.Format == ImageFormat.Rgb8)
+        {
+            throw new Exception($"Unsupported pixel format ({_ddsImageFile.Format})");
+        }
+        else
+        {
+            throw new Exception($"Unsupported pixel format ({_ddsImageFile.Format})");
+        }
+    }
+
+    private static void SaveNewFile<T>(string file, Image<T> image)
+        where T : unmanaged, IPixel<T>
+    {
+        string extension = Path.GetExtension(file);
+        if (extension == ".png")
+        {
+            image.Save(file, new PngEncoder()
+            {
+                CompressionLevel = PngCompressionLevel.DefaultCompression, // default
+            });
+        }
+        else if (extension == ".jpg")
+        {
+            image.Save(file, new JpegEncoder()
+            {
+                Quality = 85,
+            });
+        }
+        else
+        {
+            throw new Exception($"Unsupported file extension ({extension})");
+        }
+    }
+
+    private void Save<T>(string file)
+        where T : unmanaged, IPixel<T>
+    {
+        using Image<T> image = Image.LoadPixelData<T>(_ddsImageFile.Data, _ddsImageFile.Width, _ddsImageFile.Height);
+
+        SaveNewFile(file, image);
+    }
+
+    private void Save<T>(string file, Point point, Size size)
+        where T : unmanaged, IPixel<T>
+    {
+        using Image<T> image = Image.LoadPixelData<T>(_ddsImageFile.Data, _ddsImageFile.Width, _ddsImageFile.Height);
+
+        image.Mutate(x => x.Crop(new Rectangle(point, size)));
+
+        SaveNewFile(file, image);
+    }
+
+    private void SaveAsGif<T>(string file, Size size, Size innerSize, int frames, int frameDelay)
+        where T : unmanaged, IPixel<T>
+    {
+        // Load full base image
+        using Image<T> image = Image.LoadPixelData<T>(_ddsImageFile.Data, _ddsImageFile.Width, _ddsImageFile.Height);
+        using Image<T> gif = new(size.Width, size.Height);
+
+        GifMetadata gifMetadata = gif.Metadata.GetGifMetadata();
+        gifMetadata.RepeatCount = 0;
+
+        for (int i = 0; i < frames; i++)
+        {
+#pragma warning disable SA1407 // Arithmetic expressions should declare precedence
+            int xPos = i % (image.Width / innerSize.Width) * innerSize.Width;
+#pragma warning restore SA1407 // Arithmetic expressions should declare precedence
+            int yPos = i / (image.Width / innerSize.Width) * innerSize.Height;
+
+            using Image<T> imagePart = image.Clone();
+
+            imagePart.Mutate(x => x.Crop(new Rectangle(new Point(xPos, yPos), size)));
+
+            GifFrameMetadata gifFrameMetadata = imagePart.Frames.RootFrame.Metadata.GetGifMetadata();
+            gifFrameMetadata.FrameDelay = frameDelay / 10;
+
+            gif.Frames.AddFrame(imagePart.Frames.RootFrame);
+        }
+
+        gif.Frames.RemoveFrame(0);
+        gif.Save(file, new GifEncoder()
+        {
+            ColorTableMode = GifColorTableMode.Local,
+        });
+    }
+
+    private void SaveAsAPNG<T>(string file, Size size, Size innerSize, int frames, int frameDelay)
+        where T : unmanaged, IPixel<T>
+    {
+        if (Path.GetExtension(file) != ".apng")
+            throw new Exception("File is not an apng");
+
+        // Load full base image
+        using Image<T> image = Image.LoadPixelData<T>(_ddsImageFile.Data, _ddsImageFile.Width, _ddsImageFile.Height);
+        using Image<T> apng = new(size.Width, size.Height);
+
+        PngMetadata pngMetadata = apng.Metadata.GetPngMetadata();
+        pngMetadata.RepeatCount = 0;
+        pngMetadata.AnimateRootFrame = false;
+
+        for (int i = 0; i < frames; i++)
+        {
+#pragma warning disable SA1407 // Arithmetic expressions should declare precedence
+            int xPos = i % (image.Width / innerSize.Width) * innerSize.Width;
+#pragma warning restore SA1407 // Arithmetic expressions should declare precedence
+            int yPos = i / (image.Width / innerSize.Width) * innerSize.Height;
+
+            using Image<T> imagePart = image.Clone();
+
+            imagePart.Mutate(x => x.Crop(new Rectangle(new Point(xPos, yPos), size)));
+
+            PngFrameMetadata pngFrameMetadata = imagePart.Frames.RootFrame.Metadata.GetPngMetadata();
+            pngFrameMetadata.FrameDelay = new Rational((uint)frameDelay, 1000, false);
+
+            apng.Frames.AddFrame(imagePart.Frames.RootFrame);
+        }
+
+        apng.Save(file, new PngEncoder()
+        {
+            ColorType = PngColorType.RgbWithAlpha,
+            CompressionLevel = PngCompressionLevel.DefaultCompression,
+            FilterMethod = PngFilterMethod.Adaptive,
+            InterlaceMethod = PngInterlaceMode.None,
+            BitDepth = PngBitDepth.Bit8,
+        });
+    }
+}
---
+++ Tests/HeroesDataParser.Tests/DDSImageTests.cs
@@ -0,0 +1,116 @@
+﻿using SixLabors.ImageSharp;
+
+namespace HeroesDataParser.Tests;
+
+[TestClass]
+public class DDSImageTests
+{
+    private readonly string _testImagesDirectory = "TestImages";
+
+    [TestMethod]
+    public void Save_SingleImage_CreatesNewFile()
+    {
+        // arrange
+        string file = Path.Combine(_testImagesDirectory, "storm_ui_icon_nova_orbitalstrike.dds");
+        using DDSImage image = new(file);
+
+        string outputFile = Path.ChangeExtension(file, ".png");
+
+        // act
+        image.Save(outputFile);
+
+        // assert
+        File.Exists(outputFile).Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void Save_ImageIsThreeImages_CreateThreeNewImages()
+    {
+        // arrange
+        string file = Path.Combine(_testImagesDirectory, "storm_ui_mvp_icons_rewards_loyaldefender.dds");
+
+        string blueAward = Path.ChangeExtension(file.Replace("loyaldefender", "loyaldefender_blue", StringComparison.OrdinalIgnoreCase), ".png");
+        string redAward = Path.ChangeExtension(file.Replace("loyaldefender", "loyaldefender_red", StringComparison.OrdinalIgnoreCase), ".png");
+        string goldAward = Path.ChangeExtension(file.Replace("loyaldefender", "loyaldefender_gold", StringComparison.OrdinalIgnoreCase), ".png");
+
+        using DDSImage image = new(file);
+
+        // act
+        int newWidth = 444 / 3;
+        image.Save(blueAward, new Point(0, 0), new Size(newWidth, image.Height));
+        image.Save(redAward, new Point(newWidth, 0), new Size(newWidth, image.Height));
+        image.Save(goldAward, new Point(newWidth * 2, 0), new Size(newWidth, image.Height));
+
+        // assert
+        // original image size
+        image.Height.Should().Be(148);
+        image.Width.Should().Be(444);
+
+        // new files created
+        File.Exists(blueAward).Should().BeTrue();
+        File.Exists(redAward).Should().BeTrue();
+        File.Exists(goldAward).Should().BeTrue();
+
+        // verify new image sizes
+        Image blueNewImage = Image.Load(blueAward);
+        blueNewImage.Height.Should().Be(148);
+        blueNewImage.Width.Should().Be(148);
+
+        Image redNewImage = Image.Load(redAward);
+        redNewImage.Height.Should().Be(148);
+        redNewImage.Width.Should().Be(148);
+
+        Image redGoldImage = Image.Load(goldAward);
+        redGoldImage.Height.Should().Be(148);
+        redGoldImage.Width.Should().Be(148);
+    }
+
+    [TestMethod]
+    public void SaveAsGif_ImageWithMultipleImages_CreateNewFile()
+    {
+        // arrange
+        string file = Path.Combine(_testImagesDirectory, "storm_emoji_cat_gleam_anim_sheet.dds");
+        using DDSImage image = new(file);
+
+        string outputFile = Path.ChangeExtension(file, "gif");
+
+        // act
+        image.SaveAsGif(outputFile, new Size(34, 32), new Size(40, 32), 25, 50);
+
+        // assert
+        File.Exists(outputFile).Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void SaveAsAPNG_ImageWithMultipleImages_CreateNewFile()
+    {
+        // arrange
+        string file = Path.Combine(_testImagesDirectory, "storm_emoji_cat_gleam_anim_sheet.dds");
+        using DDSImage image = new(file);
+
+        string outputFile = Path.ChangeExtension(file, "apng");
+
+        // act
+        image.SaveAsAPNG(outputFile, new Size(34, 32), new Size(40, 32), 25, 50);
+
+        // assert
+        File.Exists(outputFile).Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void SaveAsAPNG_SprayImage_CreateNewFile()
+    {
+        // arrange
+        string file = Path.Combine(_testImagesDirectory, "storm_lootspray_animated_snowglobe_cursed.dds");
+        using Stream stream = File.OpenRead(file);
+        using DDSImage image = new(stream);
+
+        string outputFile = Path.ChangeExtension(file, "apng");
+
+        // act
+        image.SaveAsAPNG(outputFile, new Size(256, 256), new Size(256, 256), 2, 2000);
+
+        // assert
+        File.Exists(outputFile).Should().BeTrue();
+    }
+}
